9d15409eb09c973e79a2e57d28e9d3af32812bc5
NewAmazingPVP <gamingofpat@gmail.com>
2024-01-02 14:40:25 -0500

    test

--- Assets/Scenes/Circle Move/NewBehaviourScript.cs
+++ Assets/Scenes/Circle Move/NewBehaviourScript.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public float moveSpeed = 5f; // Adjust this speed as needed
+    public float moveSpeed = 5f;
     private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Add SpriteRenderer if not already present
         if (GetComponent<SpriteRenderer>() == null)
         {
             gameObject.AddComponent<SpriteRenderer>();
             Debug.Log("SpriteRenderer added to the GameObject.");
         }
 
-        // Add Rigidbody2D if not already present
         rb = GetComponent<Rigidbody2D>();
         if (rb == null)
         {
@@ -29,11 +27,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the GameObject horizontally
         float horizontalInput = Input.GetAxis("Horizontal");
         Vector2 movement = new Vector2(horizontalInput, 0f) * moveSpeed;
         rb.velocity = movement;
-
-        // You can add more functionality here based on user input or other conditions
     }
 }
